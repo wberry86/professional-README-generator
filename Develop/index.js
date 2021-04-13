c74484851906f97410356f7cfcdc9c8234758054
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-13 18:20:12 -0500

    created template literals and html frame

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,5 @@
 const readmeDataArgs = process.argv.slice(2, process.argv.length);
-
+const [fileName, data] = readmeDataArgs;
 
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
@@ -8,14 +8,28 @@ const questions = [];
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
+const writeToFile = (fileName, data) => {
+    return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
 
-const printReadmeData = readmeDataArr => {
-    readmeDataArr.forEach(readmeData => console.log(readmeData));
-}
-    
+  <body>
+    <h1>${fileName}</h1>
+    <h2>/${data}</h2>
+  </body>
+  </html>
+  `;
+};
 
+console.log(fileName, data);
+console.log(writeToFile(fileName, data));
 
-printReadmeData(readmeDataArgs);
 
 // TODO: Create a function to initialize app
 function init() {}
@@ -23,3 +37,18 @@ function init() {}
 // Function call to initialize app
 init();
 
+
+
+
+
+
+
+
+/*
+
+const printReadmeData = readmeDataArr => {
+    readmeDataArr.forEach(readmeData => console.log(readmeData));
+}
+printReadmeData(readmeDataArgs);
+
+*/
