3c27e8f45e42a8fea2093ec09ec4be594eb7edfe
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-13 17:16:21 -0500

    added function to print data to node

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,25 @@
-// TODO: Include packages needed for this application
+const readmeDataArgs = process.argv.slice(2, process.argv.length);
 
+
+// TODO: Include packages needed for this application
+var inquirer = require('inquirer');
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+// function writeToFile(fileName, data) {}
+
+const printReadmeData = readmeDataArr => {
+    readmeDataArr.forEach(readmeData => console.log(readmeData));
+}
+    
+
+
+printReadmeData(readmeDataArgs);
 
 // TODO: Create a function to initialize app
 function init() {}
 
 // Function call to initialize app
 init();
+
