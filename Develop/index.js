0052250bef6bcfd9f4cba7542ecfa9b6d39bf652
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-14 19:40:01 -0500

    modularized code and created seperate page for html output

--- Develop/index.js
+++ Develop/index.js
@@ -1,34 +1,24 @@
-const readmeDataArgs = process.argv.slice(2, process.argv.length);
+
+const readmeDataArgs = process.argv.slice(2);
 const [fileName, data] = readmeDataArgs;
 
 // TODO: Include packages needed for this application
 var inquirer = require('inquirer');
+const fs = require('fs');
+const writeToFile = require('./src/page-template');
+
 // TODO: Create an array of questions for user input
 const questions = [];
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
-const writeToFile = (fileName, data) => {
-    return `
-  <!DOCTYPE html> 
-  <html lang="en"> 
-  <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
-  </head>
 
-  <body>
-    <h1>${fileName}</h1>
-    <h2>/${data}</h2>
-  </body>
-  </html>
-  `;
-};
 
-console.log(fileName, data);
-console.log(writeToFile(fileName, data));
+fs.writeFile('index.html', writeToFile(fileName, data), err => {
+    if (err) throw err;
+
+    console.log('Readme complete! Check out index.html to see the output!');
+});
 
 
 // TODO: Create a function to initialize app
