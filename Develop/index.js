414147eec51c11373ebddc2363dc7696267000e6
William Berry <76190311+wberry86@users.noreply.github.com>
2021-04-14 22:42:16 -0500

    added screenshot for readme and updated template

--- Develop/index.js
+++ Develop/index.js
@@ -1,8 +1,8 @@
 // TODO: Include packages needed for this application
 const inquirer = require("inquirer");
 
-//const fs = require('fs');
-//const writeToFile = require('./src/page-template');
+const fs = require('fs');
+const writeToFile = require('./src/page-template');
 
 // TODO: Create an array of questions for user input
 const questions = [];
@@ -91,7 +91,19 @@ const promptUser = () => {
   ]);
 };
 
-promptUser().then((answers) => console.log(answers));
+promptUser()
+  .then(portfolioData => {
+    const pageHTML = writeToFile(portfolioData);
+
+     fs.writeFile('./index.html', pageHTML, err => {
+       if (err) throw new Error(err);
+
+       console.log('Readme complete! Check out index.html in this directory to see it!');
+    });
+  });
+
+
+//promptUser().then((answers) => console.log(answers));
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}
--- Develop/src/page-template.js
+++ Develop/src/page-template.js
@@ -1,20 +1,64 @@
-const writeToFile = (fileName, data) => {
+module.exports = templateData => {
+    const { content, ...header } = templateData;
+
     return `
+  <!DOCTYPE html>
+  <html lang="en">
+
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css">
+    <link href="https://fonts.googleapis.com/css?family=Public+Sans:300i,300,500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style.css">
+  </head>
+
+  <body>
+    <header>
+      <div class="container flex-row justify-space-between align-center py-3">
+        <h1 class="page-title text-secondary bg-dark py-2 px-3">${header.fileName}</h1>
+        <nav class="flex-row">
+          <h2 class="ml-2 my-1 px-2 py-1 bg-secondary text-dark">${header.data}</h2>
+        </nav>
+      </div>
+    </header>
+    <main class="container my-5">
+
+    </main>
+    <footer class="container text-center py-3">
+      <h3 class="text-dark">&copy; ${new Date().getFullYear()} by ${header.fileName}</h3>
+    </footer>
+  </body>
+  </html>
+  `;
+};
+
+
+
+
+
+
+
+
+
+/*
+return `
   <!DOCTYPE html> 
   <html lang="en"> 
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Portfolio Demo</title>
+    <title>Professional README Generator</title>
   </head>
 
   <body>
-    <h1>${fileName}</h1>
-    <h2>${data}</h2>
+    <h1>${templateData.fileName}</h1>
+    <h2>${templateData.data}</h2>
   </body>
   </html>
   `;
-};
 
-module.exports = writeToFile;
+  */
